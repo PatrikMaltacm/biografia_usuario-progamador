9e8b58e543815fe05598444663bc5e67fa3673d9
Patrik Malta <maltapatrik3@gmail.com>
2024-02-19 22:25:11 -0300

    Atualização imagem perfil

--- bio-usuario-prog/src/App.jsx
+++ bio-usuario-prog/src/App.jsx
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className='root'>
         <h1>usuario_progamador</h1>
-        <img src="https://i0.wp.com/doutorespalhacos.com.br/wp-content/uploads/2020/03/logo-instagram-png-fundo-transparente.png?ssl=1" alt="Imagem do perfil" srcset="" />
+        <img src="https://scontent.xx.fbcdn.net/v/t1.15752-9/428068459_956590982634762_5988270829488893557_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=510075&_nc_ohc=620bK3QbVOwAX_KGkwU&_nc_ad=z-m&_nc_cid=0&_nc_ht=scontent.xx&oh=03_AdQ0p4e7mm1E5HZbcWHwEjFKlGW1FKRgRWsKb004CGJW3Q&oe=65FB8BFE" alt="Imagem do perfil" srcset="" />
         <h2>Ebook dos primeiros conhecimentos sobre desenvolvimento Frontend</h2>
         <h2 className='emoji'>👇</h2>
         <a href="https://docs.google.com/document/d/11Hh85U9l7ynsjgvXBOtlsWtdz_FNJV2BVV2arHAGDQo/edit?usp=sharing">Acesse o Ebook aqui</a>
