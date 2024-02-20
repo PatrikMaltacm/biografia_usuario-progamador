5d5d7f6604e0f5ceb98cd341dadae87b9fe1e19b
Patrik Malta <maltapatrik3@gmail.com>
2024-02-19 21:12:08 -0300

    Primeiro commit

---
+++ bio-usuario-prog/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <div className='root'>
+        <h1>usuario_progamador</h1>
+        <img src="https://i0.wp.com/doutorespalhacos.com.br/wp-content/uploads/2020/03/logo-instagram-png-fundo-transparente.png?ssl=1" alt="Imagem do perfil" srcset="" />
+        <h2>Ebook dos primeiros conhecimentos sobre desenvolvimento Frontend</h2>
+        <h2 className='emoji'>👇</h2>
+        <a href="https://docs.google.com/document/d/11Hh85U9l7ynsjgvXBOtlsWtdz_FNJV2BVV2arHAGDQo/edit?usp=sharing">Acesse o Ebook aqui</a>
+        
+        <p>Tudo aqui foi feito com o que eu aprendi de ReactJS</p>
+        <p>&copy; 2024 usuario_progamador</p>
+    </div>
+  )
+}
+
+export default App
