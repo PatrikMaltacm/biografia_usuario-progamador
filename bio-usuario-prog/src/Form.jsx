f2a70c295c9df8e9d25a6eb67d92af23defac665
Patrik Malta <maltapatrik3@gmail.com>
2024-02-20 22:32:36 -0300

    dev dia 20/02/2024

--- bio-usuario-prog/src/Form.jsx
+++ bio-usuario-prog/src/Form.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "./Form.css"
 
 
 const form = () =>{
     return(
-        <div>
-            <h1>Form</h1>
+        <div className="container">
+            <h1>Preencha o formulario abaixo</h1>
+            <h3>para receber o ebook direto no seu email</h3>
             <nav>
                 <Link to="/biografia_usuario-progamador/">Home</Link>
             </nav>
